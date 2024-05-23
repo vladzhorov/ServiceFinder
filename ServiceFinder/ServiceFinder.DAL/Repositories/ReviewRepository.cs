3a7c51cff58823c8065bd5297df2e7cbcd8fe006
vladzhorov <zhorovv7@gmail.com>
2024-05-23 11:49:32 +0300

    Add repositories

---
+++ ServiceFinder/ServiceFinder.DAL/Repositories/ReviewRepository.cs
@@ -0,0 +1,12 @@
+﻿using ServiceFinder.DAL.Entites;
+using ServiceFinder.DAL.Interfaces;
+
+namespace ServiceFinder.DAL.Repositories
+{
+    public class ReviewRepository : Repository<ReviewEntity>, IReviewRepository
+    {
+        public ReviewRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ ServiceFinder/ServiceFinder.DAL/Repositories/ServiceCategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using ServiceFinder.DAL.Entites;
+using ServiceFinder.DAL.Interfaces;
+
+namespace ServiceFinder.DAL.Repositories
+{
+    public class ServiceCategoryRepository : Repository<ServiceCategoryEntity>, IServiceCategoryRepository
+    {
+        public ServiceCategoryRepository(AppDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
