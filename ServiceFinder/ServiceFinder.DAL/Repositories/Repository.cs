727ed1fb545d2fe742043197c3b913c08565ffe2
vladzhorov <zhorovv7@gmail.com>
2024-05-24 12:24:40 +0300

    Add property to access

--- ServiceFinder/ServiceFinder.DAL/Repositories/Repository.cs
+++ ServiceFinder/ServiceFinder.DAL/Repositories/Repository.cs
@@ -7,7 +7,7 @@ namespace ServiceFinder.DAL.Repositories
     public class Repository<T> : IRepository<T> where T : class
     {
         protected readonly AppDbContext _dbContext;
-
+        protected DbSet<T> Query => _dbContext.Set<T>();
         public Repository(AppDbContext dbContext)
         {
             _dbContext = dbContext;
@@ -15,18 +15,18 @@ namespace ServiceFinder.DAL.Repositories
 
         public virtual Task<List<T>> GetAllAsync(CancellationToken cancellationToken)
         {
-            return _dbContext.Set<T>().ToListAsync(cancellationToken);
+            return Query.ToListAsync(cancellationToken);
         }
 
         public virtual Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
         {
-            return _dbContext.Set<T>().FindAsync(new object?[] { id, cancellationToken }, cancellationToken: cancellationToken).AsTask();
+            return Query.FindAsync(new object?[] { id, cancellationToken }, cancellationToken: cancellationToken).AsTask();
         }
 
 
         public async virtual Task<T> AddAsync(T entity, CancellationToken cancellationToken)
         {
-            await _dbContext.Set<T>().AddAsync(entity, cancellationToken);
+            await Query.AddAsync(entity, cancellationToken);
             await _dbContext.SaveChangesAsync(cancellationToken);
 
             return entity;
@@ -34,7 +34,7 @@ namespace ServiceFinder.DAL.Repositories
 
         public async virtual Task<T> UpdateAsync(T entity, CancellationToken cancellationToken)
         {
-            _dbContext.Set<T>().Update(entity);
+            Query.Update(entity);
             await _dbContext.SaveChangesAsync(cancellationToken);
 
             return entity;
@@ -42,13 +42,13 @@ namespace ServiceFinder.DAL.Repositories
 
         public async virtual Task DeleteAsync(T entity, CancellationToken cancellationToken)
         {
-            _dbContext.Set<T>().Remove(entity);
+            Query.Remove(entity);
             await _dbContext.SaveChangesAsync(cancellationToken);
         }
 
         public async virtual Task<IEnumerable<T>> GetByPredicateAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken)
         {
-            return await _dbContext.Set<T>().Where(predicate).ToListAsync(cancellationToken);
+            return await Query.Where(predicate).ToListAsync(cancellationToken);
         }
     }
 }
