afe3d336238b5c6ff618d163fe44df590a80ae12
vladzhorov <zhorovv7@gmail.com>
2024-05-28 02:03:20 +0300

    Add Interfaces

---
+++ ServiceFinder/ServiceFinder.BLL/Abstarctions/Services/IGenericService.cs
@@ -0,0 +1,13 @@
+﻿namespace ServiceFinder.BLL.Abstractions.Services
+{
+    public interface IGenericService<TEntity, TModel>
+    {
+        Task<TModel> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<TModel> CreateAsync(TModel model, CancellationToken cancellationToken);
+        Task<TModel> UpdateAsync(TModel model, CancellationToken cancellationToken);
+        Task DeleteAsync(Guid id, CancellationToken cancellationToken);
+        Task<List<TModel>> GetAllAsync(CancellationToken cancellationToken);
+
+    }
+}
+
---
+++ ServiceFinder/ServiceFinder.BLL/Abstarctions/Services/IReviewService.cs
@@ -0,0 +1,10 @@
+﻿using ServiceFinder.BLL.Abstractions.Services;
+using ServiceFinder.BLL.Models;
+using ServiceFinder.DAL.Entites;
+
+namespace ServiceFinder.BLL.Abstarctions.Services
+{
+    public interface IReviewService : IGenericService<ReviewEntity, Review>
+    {
+    }
+}
