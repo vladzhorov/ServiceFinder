336c0a984b985dca82fb550eca2ddffc9e073845
vladzhorov <zhorovv7@gmail.com>
2024-05-28 02:01:59 +0300

    Configure DAL-BLL mapping

---
+++ ServiceFinder/ServiceFinder.BLL/BLLServiceExtension.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceFinder.BLL.Abstarctions.Services;
+using ServiceFinder.BLL.Mapper;
+using ServiceFinder.BLL.Services;
+
+
+namespace ServiceFinder.BLL
+{
+    public static class BLLServiceExtension
+    {
+        public static void AddBLLDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAutoMapper(typeof(Mapping));
+            services.AddScoped<IUserProfileService, UserProfileService>();
+            services.AddScoped<IAssistanceService, AssistanceService>();
+            services.AddScoped<IReviewService, ReviewService>();
+            services.AddScoped<IAssistanceCategoryService, AssistanceCategoryService>();
+        }
+    }
+}
+
---
+++ ServiceFinder/ServiceFinder.BLL/Mapper/Mapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using ServiceFinder.BLL.Models;
+using ServiceFinder.DAL.Entites;
+
+namespace ServiceFinder.BLL.Mapper
+{
+    internal class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<UserProfileEntity, UserProfile>().ReverseMap();
+            CreateMap<ReviewEntity, Review>().ReverseMap();
+            CreateMap<ServiceEntity, Service>().ReverseMap();
+            CreateMap<ServiceCategoryEntity, ServiceCategory>().ReverseMap();
+        }
+    }
+}
