9afc9dc377dae8239ec422f46aaf3f960f60a03c
vladzhorov <zhorovv7@gmail.com>
2024-05-30 02:06:47 +0300

    Create ApiRouteConstants

---
+++ ServiceFinder/ServiceFinder.API/DI/ApiRoutes.cs
@@ -0,0 +1,10 @@
+﻿namespace ServiceFinder.API.DI
+{
+    public class ApiRoutes
+    {
+        public const string assistancesCategory = "api/assistancesCategory";
+        public const string assistances = "api/assistances";
+        public const string reviews = "api/reviews";
+        public const string usersProfile = "api/usersProfile";
+    }
+}
