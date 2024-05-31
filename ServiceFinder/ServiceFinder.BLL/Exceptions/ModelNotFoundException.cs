7a3a3693ccfdd5ad41d210d97d6c7c71aa1c3e37
vladzhorov <zhorovv7@gmail.com>
2024-05-31 15:37:14 +0300

    Create Custom Exceptions for ExceptionMiddleware

---
+++ ServiceFinder/ServiceFinder.BLL/Exceptions/ModelNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿namespace ServiceFinder.BLL.Exceptions
+{
+    public class ModelNotFoundException : Exception
+    {
+        public int StatusCode { get; } = 404;
+        public int ModelId { get; }
+
+        public ModelNotFoundException() { }
+
+        public ModelNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+
+        public ModelNotFoundException(string message) : base(message) { }
+
+        public ModelNotFoundException(int modelId, string message) : base(message)
+        {
+            ModelId = modelId;
+        }
+        public ModelNotFoundException(int modelId) : base($"Model with Id={modelId} doesn't exist.")
+        {
+            ModelId = modelId;
+        }
+    }
+}
