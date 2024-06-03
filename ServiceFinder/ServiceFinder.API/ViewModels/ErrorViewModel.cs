4d5cbe5a37392f0c591ab3ff0b83655ed0663165
vladzhorov <zhorovv7@gmail.com>
2024-06-03 18:15:56 +0300

    fix null state warning

--- ServiceFinder/ServiceFinder.API/ViewModels/ErrorViewModel.cs
+++ ServiceFinder/ServiceFinder.API/ViewModels/ErrorViewModel.cs
@@ -5,7 +5,7 @@ namespace ServiceFinder.API.ViewModels
     public class ErrorViewModel
     {
         public int ErrorCode { get; set; }
-        public string Message { get; set; }
+        public string? Message { get; set; }
         public override string ToString()
         {
             return JsonSerializer.Serialize(this);
