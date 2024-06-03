0318cd94193987a6db3bd9c44d45b9791d194e5a
vladzhorov <zhorovv7@gmail.com>
2024-06-03 13:53:06 +0300

    Create FluentValidatorException

---
+++ ServiceFinder/ServiceFinder.API/Exceptions/FluentValidatorException.cs
@@ -0,0 +1,14 @@
+﻿namespace ServiceFinder.API.Exceptions
+{
+
+    public class FluentValidatorException : Exception
+    {
+        public int StatusCode { get; } = 400;
+
+        public FluentValidatorException() { }
+
+        public FluentValidatorException(string message, Exception innerException) : base(message, innerException) { }
+
+        public FluentValidatorException(string message) : base(message) { }
+    }
+}
