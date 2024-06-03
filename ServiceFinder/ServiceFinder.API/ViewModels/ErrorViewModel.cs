3236d6778afa89b47337a2e4c9fbb080c7996ffc
vladzhorov <zhorovv7@gmail.com>
2024-06-03 18:09:06 +0300

    Add field ErrorCode Instead StatusCode

--- ServiceFinder/ServiceFinder.API/ViewModels/ErrorViewModel.cs
+++ ServiceFinder/ServiceFinder.API/ViewModels/ErrorViewModel.cs
@@ -4,7 +4,7 @@ namespace ServiceFinder.API.ViewModels
 {
     public class ErrorViewModel
     {
-        public int StatusCode { get; set; }
+        public int ErrorCode { get; set; }
         public string Message { get; set; }
         public override string ToString()
         {
