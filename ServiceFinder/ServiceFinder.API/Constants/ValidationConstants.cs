1ee514320da8f4b5551ac9b14df169f9262f8f01
vladzhorov <zhorovv7@gmail.com>
2024-06-05 14:10:32 +0300

    Update ValidationConstant DurationInMinutes

--- ServiceFinder/ServiceFinder.API/Constants/ValidationConstants.cs
+++ ServiceFinder/ServiceFinder.API/Constants/ValidationConstants.cs
@@ -8,6 +8,6 @@
         public const int MaximumRating = 5;
         public const string PhoneNumberPattern = @"8\(0(29|44|33|25)\)\d{3}-\d{2}-\d{2}";
         public const decimal MinimumPrice = 0;
-        public const int MinimumMinutes = 0;
+        public const int MinimumDurationInMinutes = 0;
     }
 }
