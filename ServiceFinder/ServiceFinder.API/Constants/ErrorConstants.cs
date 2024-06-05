ecfecf1bdd4101aa687a63fca8a44db0055afda7
vladzhorov <zhorovv7@gmail.com>
2024-06-05 12:38:38 +0300

    Rename ErrorConstants to ErrorCodesConstants

--- ServiceFinder/ServiceFinder.API/Constants/ErrorConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ServiceFinder.API.Constants
-{
-    public static class ErrorConstants
-    {
-        public const string ModelNotFoundError = "model_not_found_exception";
-        public const string ValidationError = "validation_exception";
-        public const string InternalServerError = "internal_server_error_exception";
-    }
-}
