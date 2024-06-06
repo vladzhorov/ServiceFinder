1b44dfe48215d8b588c35574edb0b332d1ebf3d1
vladzhorov <zhorovv7@gmail.com>
2024-06-06 17:39:08 +0300

    Add MigrationHistory

---
+++ ServiceFinder/ServiceFinder.DAL/Migrations/20240605135716_AddUserPROFILEnAVIGATION.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServiceFinder.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPROFILEnAVIGATION : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_UserProfile_UserProfileEntityId",
+                table: "Reviews");
+
+            migrationBuilder.RenameColumn(
+                name: "UserProfileEntityId",
+                table: "Reviews",
+                newName: "UserProfileId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Reviews_UserProfileEntityId",
+                table: "Reviews",
+                newName: "IX_Reviews_UserProfileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_UserProfile_UserProfileId",
+                table: "Reviews",
+                column: "UserProfileId",
+                principalTable: "UserProfile",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_UserProfile_UserProfileId",
+                table: "Reviews");
+
+            migrationBuilder.RenameColumn(
+                name: "UserProfileId",
+                table: "Reviews",
+                newName: "UserProfileEntityId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Reviews_UserProfileId",
+                table: "Reviews",
+                newName: "IX_Reviews_UserProfileEntityId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_UserProfile_UserProfileEntityId",
+                table: "Reviews",
+                column: "UserProfileEntityId",
+                principalTable: "UserProfile",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ ServiceFinder/ServiceFinder.DAL/Migrations/20240606120116_UpdateCategoryToAssistanceCategory.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServiceFinder.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCategoryToAssistanceCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assistances_AssistanceCategories_CategoryId",
+                table: "Assistances");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Assistances_CategoryId",
+                table: "Assistances");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Assistances");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assistances_AssistanceCategoryId",
+                table: "Assistances",
+                column: "AssistanceCategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assistances_AssistanceCategories_AssistanceCategoryId",
+                table: "Assistances",
+                column: "AssistanceCategoryId",
+                principalTable: "AssistanceCategories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assistances_AssistanceCategories_AssistanceCategoryId",
+                table: "Assistances");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Assistances_AssistanceCategoryId",
+                table: "Assistances");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CategoryId",
+                table: "Assistances",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assistances_CategoryId",
+                table: "Assistances",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assistances_AssistanceCategories_CategoryId",
+                table: "Assistances",
+                column: "CategoryId",
+                principalTable: "AssistanceCategories",
+                principalColumn: "Id");
+        }
+    }
+}
