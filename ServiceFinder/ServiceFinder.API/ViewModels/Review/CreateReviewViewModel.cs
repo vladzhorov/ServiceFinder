8f09d572f545e2905dcf5bc75ddfc62d5565a86a
vladzhorov <zhorovv7@gmail.com>
2024-06-06 17:30:21 +0300

    Add UserProfile navigation field

--- ServiceFinder/ServiceFinder.API/ViewModels/Review/CreateReviewViewModel.cs
+++ ServiceFinder/ServiceFinder.API/ViewModels/Review/CreateReviewViewModel.cs
@@ -2,6 +2,7 @@
 {
     public class CreateReviewViewModel
     {
+        public Guid UserProfileId { get; set; }
         public Guid AssistanceId { get; set; }
         public float Rating { get; set; }
         public string? Comment { get; set; }
