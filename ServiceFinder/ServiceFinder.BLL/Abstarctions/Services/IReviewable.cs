7db021d6fd5b2ad26894cf32175b6890f902a3dc
vladzhorov <zhorovv7@gmail.com>
2024-06-24 12:35:56 +0300

    Add ratingCalculator with interface

---
+++ ServiceFinder/ServiceFinder.BLL/Abstarctions/Services/IReviewable.cs
@@ -0,0 +1,9 @@
+﻿using ServiceFinder.BLL.Models;
+
+namespace ServiceFinder.BLL.Abstarctions.Services
+{
+    public interface IReviewable
+    {
+        public delegate ICollection<Review> ReviewsGetter<T>(T item);
+    }
+}
