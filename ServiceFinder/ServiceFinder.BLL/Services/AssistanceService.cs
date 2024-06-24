9f173d0ee44508ea30278309b3cdfb89e7919d7d
vladzhorov <zhorovv7@gmail.com>
2024-06-24 12:37:07 +0300

    Implementation of rating calculation in services

--- ServiceFinder/ServiceFinder.BLL/Services/AssistanceService.cs
+++ ServiceFinder/ServiceFinder.BLL/Services/AssistanceService.cs
@@ -4,6 +4,7 @@ using ServiceFinder.BLL.Exceptions;
 using ServiceFinder.BLL.Models;
 using ServiceFinder.DAL.Entites;
 using ServiceFinder.DAL.Interfaces;
+using ServiceFinder.DAL.PaginationObjects;
 
 namespace ServiceFinder.BLL.Services
 {
@@ -12,16 +13,38 @@ namespace ServiceFinder.BLL.Services
         private readonly IUserProfileRepository _userProfileRepository;
         private readonly IAssistanceCategoryRepository _assistanceCategoryRepository;
 
-        public AssistanceService(IAssistanceRepository repository,
-                                 IUserProfileRepository userProfileRepository,
-                                 IAssistanceCategoryRepository assistanceCategoryRepository,
-                                 IMapper mapper)
-            : base(repository, mapper)
+        public AssistanceService(IAssistanceRepository repository, IUserProfileRepository userProfileRepository, IAssistanceCategoryRepository assistanceCategoryRepository, IMapper mapper) : base(repository, mapper)
         {
             _userProfileRepository = userProfileRepository;
             _assistanceCategoryRepository = assistanceCategoryRepository;
         }
+        public override async Task<Assistance> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var assistanceEntity = await _repository.GetByIdAsync(id, cancellationToken);
+            var assistance = _mapper.Map<Assistance>(assistanceEntity);
 
+            if (assistance.Reviews != null)
+            {
+                assistance.Rating = RatingCalculatorService.CalculateRating(assistance, x => x.Reviews);
+            }
+
+            return assistance;
+        }
+
+        public override async Task<PagedResult<Assistance>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken)
+        {
+            var pagedResult = await _repository.GetAllAsync(pageNumber, pageSize, cancellationToken);
+            var assistances = _mapper.Map<PagedResult<Assistance>>(pagedResult);
+
+            foreach (var assistance in assistances.Data)
+            {
+                if (assistance.Reviews != null)
+                {
+                    assistance.Rating = RatingCalculatorService.CalculateRating(assistance, x => x.Reviews);
+                }
+            }
+            return assistances;
+        }
         public async Task<bool> IsUserProfileExistsAsync(Guid userProfileId, CancellationToken cancellationToken)
         {
             var userProfile = await _userProfileRepository.GetByIdAsync(userProfileId, cancellationToken);
--- ServiceFinder/ServiceFinder.BLL/Services/UserProfileService.cs
+++ ServiceFinder/ServiceFinder.BLL/Services/UserProfileService.cs
@@ -3,14 +3,58 @@ using ServiceFinder.BLL.Abstarctions.Services;
 using ServiceFinder.BLL.Models;
 using ServiceFinder.DAL.Entites;
 using ServiceFinder.DAL.Interfaces;
-
+using ServiceFinder.DAL.PaginationObjects;
 
 namespace ServiceFinder.BLL.Services
 {
     public class UserProfileService : GenericService<UserProfileEntity, UserProfile>, IUserProfileService
     {
+        private readonly IUserProfileRepository _userProfileRepository;
+
         public UserProfileService(IUserProfileRepository userProfileRepository, IMapper mapper) : base(userProfileRepository, mapper)
         {
+            _userProfileRepository = userProfileRepository;
+        }
+
+        public override async Task<UserProfile> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var entity = await _userProfileRepository.GetByIdAsync(id, cancellationToken);
+            var userProfile = _mapper.Map<UserProfile>(entity);
+
+            if (userProfile.Assistances != null)
+            {
+                foreach (var assistance in userProfile.Assistances)
+                {
+                    if (assistance.Reviews != null)
+                    {
+                        assistance.Rating = RatingCalculatorService.CalculateRating(assistance, x => x.Reviews);
+                    }
+                }
+            }
+            return userProfile;
+        }
+
+        public override async Task<PagedResult<UserProfile>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken)
+        {
+            var pagedResult = await _userProfileRepository.GetAllAsync(pageNumber, pageSize, cancellationToken);
+            var userProfiles = _mapper.Map<PagedResult<UserProfile>>(pagedResult);
+
+            foreach (var userProfile in userProfiles.Data)
+            {
+                if (userProfile.Assistances != null)
+                {
+                    foreach (var assistance in userProfile.Assistances)
+                    {
+                        if (assistance.Reviews != null)
+                        {
+                            assistance.Rating = RatingCalculatorService.CalculateRating(assistance, x => x.Reviews);
+                        }
+                    }
+                }
+            }
+            return userProfiles;
         }
     }
 }
+
+
