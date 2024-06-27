b5594ae8d8b4b368b69085fec5fba81272a006c3
vladzhorov <zhorovv7@gmail.com>
2024-06-27 13:02:00 +0300

    Add ApiRoutes constants

---
+++ ServiceFinder/ServiceFinder.IntegrationTest/Constants/ApiRoutes.cs
@@ -0,0 +1,11 @@
+﻿namespace ServiceFinder.IntegrationTest.Constants
+{
+    public static class ApiRoutes
+    {
+        public const string Api = "/api";
+        public const string AssistanceCategories = Api + "/assistancesCategory";
+        public const string Assistances = Api + "/assistances";
+        public const string Reviews = Api + "/reviews";
+        public const string UserProfiles = Api + "/usersProfile";
+    }
+}
