70113b10ac298b63a8900338adc57bee77242cbb
vladzhorov <zhorovv7@gmail.com>
2024-06-27 13:01:31 +0300

    Add Integration test helpers

---
+++ ServiceFinder/ServiceFinder.IntegrationTest/TestHelpers/ReviewTestHelper.cs
@@ -0,0 +1,63 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceFinder.API.ViewModels.Review;
+using ServiceFinder.DAL;
+using ServiceFinder.DAL.PaginationObjects;
+
+namespace ServiceFinder.IntegrationTests.TestHelpers
+{
+    public class ReviewTestHelper
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public ReviewTestHelper(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        public static CreateReviewViewModel CreateReviewViewModel()
+        {
+            return new CreateReviewViewModel
+            {
+                AssistanceId = Guid.NewGuid(),
+                UserProfileId = Guid.NewGuid(),
+                Rating = 4.5f,
+                Comment = "Excellent service!"
+            };
+        }
+
+
+
+        public void AssertPagedResult(PagedResult<ReviewViewModel> result, int expectedPageNumber, int expectedPageSize)
+        {
+            result.Should().NotBeNull();
+            result.PageNumber.Should().Be(expectedPageNumber);
+            result.PageSize.Should().Be(expectedPageSize);
+        }
+
+        public void AssertReview(ReviewViewModel viewModel, Guid expectedId)
+        {
+            viewModel.Should().NotBeNull();
+            viewModel.Id.Should().Be(expectedId);
+        }
+
+        public void AssertCreatedReview(ReviewViewModel viewModel, CreateReviewViewModel createModel)
+        {
+            viewModel.Should().NotBeNull();
+            viewModel.UserProfileId.Should().Be(createModel.UserProfileId);
+            viewModel.AssistanceId.Should().Be(createModel.AssistanceId);
+            viewModel.Rating.Should().Be(createModel.Rating);
+            viewModel.Comment.Should().Be(createModel.Comment);
+        }
+
+        public void AssertReviewDeleted(Guid reviewId)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                var review = dbContext.Reviews.FirstOrDefault(r => r.Id == reviewId);
+                review.Should().BeNull();
+            }
+        }
+    }
+}
---
+++ ServiceFinder/ServiceFinder.IntegrationTest/TestHelpers/UserProfileTestHelper.cs
@@ -0,0 +1,75 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceFinder.API.ViewModels.UserProfile;
+using ServiceFinder.DAL;
+using ServiceFinder.DAL.PaginationObjects;
+
+namespace ServiceFinder.IntegrationTests.TestHelpers
+{
+    public class UserProfileTestHelper
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+
+        public UserProfileTestHelper(IServiceScopeFactory scopeFactory)
+        {
+            _scopeFactory = scopeFactory;
+        }
+
+        public void AssertPagedResult(PagedResult<UserProfileViewModel> result, int pageNumber, int pageSize)
+        {
+            result.Should().NotBeNull();
+            result.Data.Should().NotBeNullOrEmpty();
+            result.PageNumber.Should().Be(pageNumber);
+            result.PageSize.Should().Be(pageSize);
+        }
+
+        public void AssertUserProfile(UserProfileViewModel result, Guid id)
+        {
+            result.Should().NotBeNull();
+            result.Id.Should().Be(id);
+        }
+
+        public void AssertCreatedUserProfile(UserProfileViewModel result, CreateUserProfileViewModel viewModel)
+        {
+            result.Should().NotBeNull();
+            result.PhotoURL.Should().Be(viewModel.PhotoURL);
+            result.PhoneNumber.Should().Be(viewModel.PhoneNumber);
+        }
+
+        public void AssertUpdatedUserProfile(UserProfileViewModel result, Guid id, UpdateUserProfileViewModel viewModel)
+        {
+            result.Should().NotBeNull();
+            result.Id.Should().Be(id);
+            result.PhotoURL.Should().Be(viewModel.PhotoURL);
+            result.PhoneNumber.Should().Be(viewModel.PhoneNumber);
+        }
+
+        public void AssertUserProfileDeleted(Guid id)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                var deletedUserProfile = dbContext.UserProfile.Find(id);
+                deletedUserProfile.Should().BeNull();
+            }
+        }
+
+        public static CreateUserProfileViewModel CreateUserProfileViewModel()
+        {
+            return new CreateUserProfileViewModel
+            {
+                PhotoURL = "new/photo/url",
+                PhoneNumber = "8(029)111-45-67",
+            };
+        }
+
+        public static UpdateUserProfileViewModel UpdateUserProfileViewModel()
+        {
+            return new UpdateUserProfileViewModel
+            {
+                PhotoURL = "updated/photo/url",
+                PhoneNumber = "8(029)121-45-67",
+            };
+        }
+    }
+}
