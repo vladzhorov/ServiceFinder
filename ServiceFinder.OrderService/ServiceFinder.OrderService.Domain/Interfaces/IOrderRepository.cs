3d9f4f1d5720a04c6fb53d20a2f79c0af9b5b972
vladzhorov <zhorovv7@gmail.com>
2024-07-01 19:55:56 +0300

    add order interfaces

---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Domain/Interfaces/IOrderRepository.cs
@@ -0,0 +1,14 @@
+﻿using ServiceFinder.Domain.PaginationObjects;
+using ServiceFinder.OrderService.Domain.Models;
+
+namespace OrderService.Domain.Interfaces
+{
+    public interface IOrderRepository
+    {
+        Task<Order> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<PagedResult<Order>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken);
+        Task AddAsync(Order order, CancellationToken cancellationToken);
+        Task UpdateAsync(Order order, CancellationToken cancellationToken);
+        Task DeleteAsync(Guid id, CancellationToken cancellationToken);
+    }
+}
---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Domain/Interfaces/IOrderRequestRepository.cs
@@ -0,0 +1,13 @@
+﻿using ServiceFinder.Domain.PaginationObjects;
+using ServiceFinder.OrderService.Domain.Models;
+
+namespace OrderService.Domain.Interfaces
+{
+    public interface IOrderRequestRepository
+    {
+        Task<OrderRequest> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<PagedResult<OrderRequest>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken);
+        Task AddAsync(OrderRequest orderRequest, CancellationToken cancellationToken);
+        Task UpdateAsync(OrderRequest orderRequest, CancellationToken cancellationToken);
+    }
+}
