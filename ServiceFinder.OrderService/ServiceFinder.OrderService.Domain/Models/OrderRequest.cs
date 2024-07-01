3c50b127a8a091462c93f3053ea8d6b7d0743149
vladzhorov <zhorovv7@gmail.com>
2024-07-01 19:54:15 +0300

    add models

---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Domain/Models/OrderRequest.cs
@@ -0,0 +1,16 @@
+﻿using ServiceFinder.OrderService.Domain.Enums;
+
+namespace ServiceFinder.OrderService.Domain.Models
+{
+    public class OrderRequest
+    {
+        public Guid Id { get; set; }
+        public Guid ServiceId { get; set; }
+        public Guid CustomerId { get; set; }
+        public int DurationInMinutes { get; set; }
+        public OrderStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
