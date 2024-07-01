7298af2594063c16bdff2b04201b084addbd5983
vladzhorov <zhorovv7@gmail.com>
2024-07-01 19:55:20 +0300

    add events

---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Domain/Events/OrderRequestStatusChangedEvent .cs
@@ -0,0 +1,14 @@
+﻿using ServiceFinder.OrderService.Domain.Models;
+
+namespace ServiceFinder.OrderService.Domain.Events
+{
+    public class OrderRequestStatusChangedEvent : IDomainEvent
+    {
+        public OrderRequest OrderRequest { get; }
+
+        public OrderRequestStatusChangedEvent(OrderRequest orderRequest)
+        {
+            OrderRequest = orderRequest;
+        }
+    }
+}
---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Domain/Events/OrderStatusChangedEvent .cs
@@ -0,0 +1,15 @@
+﻿using ServiceFinder.OrderService.Domain.Models;
+
+
+namespace ServiceFinder.OrderService.Domain.Events
+{
+    public class OrderStatusChangedEvent : IDomainEvent
+    {
+        public Order Order { get; }
+
+        public OrderStatusChangedEvent(Order order)
+        {
+            Order = order;
+        }
+    }
+}
