e1e1cb16710badb983d84c14a3dae803d69e64f6
vladzhorov <zhorovv7@gmail.com>
2024-07-02 15:09:31 +0300

    Update OrderStatusValidator.cs

--- ServiceFinder.OrderService/ServiceFinder.OrderService.Domain/Validators/OrderStatusValidator.cs
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Domain/Validators/OrderStatusValidator.cs
@@ -4,28 +4,25 @@ namespace ServiceFinder.OrderService.Domain.Validators
 {
     public static class OrderStatusValidator
     {
-        private static readonly HashSet<(OrderStatus, OrderStatus)> ValidOrderStatusTransitions = new HashSet<(OrderStatus, OrderStatus)>
+        private static readonly Dictionary<OrderStatus, OrderStatus[]> ValidOrderStatusTransitions = new Dictionary<OrderStatus, OrderStatus[]>
         {
-            (OrderStatus.Pending, OrderStatus.Confirmed),
-            (OrderStatus.Pending, OrderStatus.Cancelled),
-            (OrderStatus.Confirmed, OrderStatus.InProgress),
-            (OrderStatus.Confirmed, OrderStatus.Cancelled),
-            (OrderStatus.InProgress, OrderStatus.Completed),
-            (OrderStatus.InProgress, OrderStatus.Cancelled)
+            { OrderStatus.Pending, new OrderStatus[] { OrderStatus.Confirmed, OrderStatus.Cancelled } },
+            { OrderStatus.Confirmed, new OrderStatus[] { OrderStatus.InProgress, OrderStatus.Cancelled } },
+            { OrderStatus.InProgress, new OrderStatus[] { OrderStatus.Completed, OrderStatus.Cancelled } },
         };
 
-        private static readonly HashSet<(OrderRequestStatus, OrderRequestStatus)> ValidOrderRequestStatusTransitions = new HashSet<(OrderRequestStatus, OrderRequestStatus)>
+        private static readonly Dictionary<OrderRequestStatus, OrderRequestStatus[]> ValidOrderRequestStatusTransitions = new Dictionary<OrderRequestStatus, OrderRequestStatus[]>
         {
-            (OrderRequestStatus.Pending, OrderRequestStatus.Approved),
-            (OrderRequestStatus.Pending, OrderRequestStatus.Rejected),
-            (OrderRequestStatus.Approved, OrderRequestStatus.Cancelled)
+            { OrderRequestStatus.Pending, new OrderRequestStatus[] { OrderRequestStatus.Approved, OrderRequestStatus.Rejected } },
+            { OrderRequestStatus.Approved, new OrderRequestStatus[] { OrderRequestStatus.Cancelled } },
         };
 
         public static void ValidateStatusTransition(OrderStatus currentStatus, OrderStatus newStatus)
         {
-            if (currentStatus == newStatus) return;
+            if (currentStatus == newStatus)
+                return;
 
-            if (!ValidOrderStatusTransitions.Contains((currentStatus, newStatus)))
+            if (!ValidOrderStatusTransitions.TryGetValue(currentStatus, out var validTransitions) || Array.IndexOf(validTransitions, newStatus) == -1)
             {
                 throw new InvalidOperationException($"Invalid convert {currentStatus} to {newStatus}");
             }
@@ -33,9 +30,10 @@ namespace ServiceFinder.OrderService.Domain.Validators
 
         public static void ValidateStatusTransition(OrderRequestStatus currentStatus, OrderRequestStatus newStatus)
         {
-            if (currentStatus == newStatus) return;
+            if (currentStatus == newStatus)
+                return;
 
-            if (!ValidOrderRequestStatusTransitions.Contains((currentStatus, newStatus)))
+            if (!ValidOrderRequestStatusTransitions.TryGetValue(currentStatus, out var validTransitions) || Array.IndexOf(validTransitions, newStatus) == -1)
             {
                 throw new InvalidOperationException($"Invalid convert {currentStatus} to {newStatus}");
             }
