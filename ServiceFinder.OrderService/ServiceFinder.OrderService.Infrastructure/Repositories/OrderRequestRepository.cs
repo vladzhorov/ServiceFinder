c5eced908212e0914f86c55eb77e59d8b76b77d5
vladzhorov <zhorovv7@gmail.com>
2024-07-04 17:01:27 +0300

    add repositories

---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Infrastructure/Repositories/OrderRequestRepository.cs
@@ -0,0 +1,13 @@
+﻿using ServiceFinder.OrderService.Domain.Interfaces;
+using ServiceFinder.OrderService.Domain.Models;
+using ServiceFinder.OrderService.Infrastructure.Repositories;
+
+namespace ServiceFinder.OrderService.Infrastructure.Persistence.Repositories
+{
+    public class OrderRequestRepository : Repository<OrderRequest>, IOrderRequestRepository
+    {
+        public OrderRequestRepository(OrderDbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceFinder.Domain.PaginationObjects;
+using ServiceFinder.OrderService.Domain.Interfaces;
+using ServiceFinder.OrderService.Domain.Models;
+using System.Linq.Expressions;
+
+namespace ServiceFinder.OrderService.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : BaseModel
+    {
+        protected readonly OrderDbContext _dbContext;
+        protected DbSet<T> Query => _dbContext.Set<T>();
+        public Repository(OrderDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        protected async Task<PagedResult<T>> GetPagedResultAsync(IQueryable<T> query, int pageNumber, int pageSize, CancellationToken cancellationToken)
+        {
+            int totalCount = await query.CountAsync(cancellationToken);
+            var data = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync(cancellationToken);
+
+            return new PagedResult<T>
+            {
+                PageNumber = pageNumber,
+                PageSize = pageSize,
+                TotalCount = totalCount,
+                TotalPages = (int)Math.Ceiling(totalCount / (double)pageSize),
+                Data = data
+            };
+        }
+
+        public async virtual Task<PagedResult<T>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken)
+        {
+            IQueryable<T> query = Query;
+            return await GetPagedResultAsync(query, pageNumber, pageSize, cancellationToken);
+        }
+
+        public virtual Task<T?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return Query.FindAsync(new object?[] { id, cancellationToken }, cancellationToken: cancellationToken).AsTask();
+        }
+
+
+        public async virtual Task<T> AddAsync(T entity, CancellationToken cancellationToken)
+        {
+            await Query.AddAsync(entity, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return entity;
+        }
+
+        public async virtual Task<T> UpdateAsync(T entity, CancellationToken cancellationToken)
+        {
+            Query.Update(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return entity;
+        }
+
+        public async virtual Task DeleteAsync(T entity, CancellationToken cancellationToken)
+        {
+            Query.Remove(entity);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+
+        public async virtual Task<IEnumerable<T>> GetByPredicateAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken)
+        {
+            return await Query.Where(predicate).ToListAsync(cancellationToken);
+        }
+    }
+}
