2dddb70fd09636231e3d3f7f5dafc0e0e133b7f5
vladzhorov <zhorovv7@gmail.com>
2024-07-10 13:50:28 +0300

    Create ApplicationExtension.cs

---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Application/ApplicationExtension.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ServiceFinder.OrderService.Application.EventHandlers;
+using ServiceFinder.OrderService.Application.Messaging;
+
+namespace ServiceFinder.OrderService.Application
+{
+    public static class ApplicationExtension
+    {
+        public static void AddApplication(this IServiceCollection services)
+        {
+            services.AddSingleton<OrderRequestStatusChangedEventHandler>();
+
+            services.AddSingleton<MessagePublisher>(sp =>
+                new MessagePublisher("rabbitmq", "order_queue"));
+
+            services.AddScoped<OrderAppService>();
+        }
+    }
+}
