0c138e3d90c67a0af42e1edcf6f8188e083c6edd
vladzhorov <zhorovv7@gmail.com>
2024-07-10 16:58:38 +0300

    add interface for app services

---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Application/Interfaces/IOrderAppService.cs
@@ -0,0 +1,12 @@
+﻿using ServiceFinder.OrderService.Application.DTOs;
+using ServiceFinder.OrderService.Domain.Enums;
+
+namespace ServiceFinder.OrderService.Application.Interfaces
+{
+    public interface IOrderAppService
+    {
+        Task<OrderDto> CreateOrderAsync(OrderDto orderDTO, decimal baseRatePerMinute, int baseRateDurationInMinutes, CancellationToken cancellationToken);
+        Task UpdateOrderStatusAsync(Guid orderId, OrderStatus newStatus, CancellationToken cancellationToken);
+        Task<OrderDto> GetOrderByIdAsync(Guid id, CancellationToken cancellationToken);
+    }
+}
