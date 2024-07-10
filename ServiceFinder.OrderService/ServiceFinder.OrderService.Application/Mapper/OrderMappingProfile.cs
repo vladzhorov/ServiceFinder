c2267b5982321d712686339a51a9ba5eed2ea931
vladzhorov <zhorovv7@gmail.com>
2024-07-10 13:45:37 +0300

    Create OrderMappingProfile.cs

---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Application/Mapper/OrderMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using ServiceFinder.OrderService.Application.DTOs;
+using ServiceFinder.OrderService.Domain.Models;
+
+namespace ServiceFinder.OrderService.Application.Mapper
+{
+    public class OrderMappingProfile : Profile
+    {
+        public OrderMappingProfile()
+        {
+            CreateMap<Order, OrderDto>();
+            CreateMap<OrderDto, Order>();
+
+            CreateMap<OrderRequest, OrderRequestDto>();
+            CreateMap<OrderRequestDto, OrderRequest>();
+        }
+    }
+}
