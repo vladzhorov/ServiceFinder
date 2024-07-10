c0a3a1ed3ad1e6c95d9d22a50dd95a8d14346893
vladzhorov <zhorovv7@gmail.com>
2024-07-10 18:51:40 +0300

    add configurations

---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Application/Messaging/RabbitMQConfigurations/RabbitMQExchangeConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace ServiceFinder.OrderService.Application.Messaging.RabbitMQConfigurations
+{
+    public class RabbitMQExchangeConfiguration
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+    }
+}
