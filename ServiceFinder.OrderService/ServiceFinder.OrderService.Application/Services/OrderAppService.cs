62ae958575f085e9233d01d35321b60cf59acb79
vladzhorov <zhorovv7@gmail.com>
2024-07-10 13:50:09 +0300

    add services

---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Application/Services/OrderAppService.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using ServiceFinder.OrderService.Application.DTOs;
+using ServiceFinder.OrderService.Domain.Enums;
+using ServiceFinder.OrderService.Domain.Interfaces;
+using ServiceFinder.OrderService.Domain.Models;
+using ServiceFinder.OrderService.Domain.Services;
+
+public class OrderAppService
+{
+    private readonly OrderService _orderService;
+    private readonly IOrderRepository _orderRepository;
+    private readonly IMapper _mapper;
+
+    public OrderAppService(OrderService orderService, IOrderRepository orderRepository, IMapper mapper)
+    {
+        _orderService = orderService;
+        _orderRepository = orderRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<OrderDto> CreateOrderAsync(OrderDto orderDTO, decimal baseRatePerMinute, int baseRateDurationInMinutes, CancellationToken cancellationToken)
+    {
+        var order = _mapper.Map<Order>(orderDTO);
+        await _orderService.CreateOrderAsync(order, baseRatePerMinute, baseRateDurationInMinutes, cancellationToken);
+        return _mapper.Map<OrderDto>(order);
+    }
+
+    public async Task UpdateOrderStatusAsync(Guid orderId, OrderStatus newStatus, CancellationToken cancellationToken)
+    {
+        await _orderService.UpdateOrderStatusAsync(orderId, newStatus, cancellationToken);
+    }
+
+    public async Task<OrderDto> GetOrderByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        var order = await _orderRepository.GetByIdAsync(id, cancellationToken);
+        return _mapper.Map<OrderDto>(order);
+    }
+}
---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Application/Services/OrderRequestAppService.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using ServiceFinder.OrderService.Application.DTOs;
+using ServiceFinder.OrderService.Domain.Enums;
+using ServiceFinder.OrderService.Domain.Interfaces;
+using ServiceFinder.OrderService.Domain.Models;
+using ServiceFinder.OrderService.Domain.Services;
+namespace ServiceFinder.OrderService.Application
+{
+    public class OrderRequestAppService
+    {
+        private readonly OrderRequestService _orderRequestService;
+        private readonly IOrderRequestRepository _orderRequestRepository;
+        private readonly IMapper _mapper;
+
+        public OrderRequestAppService(OrderRequestService orderRequestService, IOrderRequestRepository orderRequestRepository, IMapper mapper)
+        {
+            _orderRequestService = orderRequestService;
+            _orderRequestRepository = orderRequestRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<OrderRequestDto> CreateOrderRequestAsync(OrderRequestDto orderRequestDTO, CancellationToken cancellationToken)
+        {
+            var orderRequest = _mapper.Map<OrderRequest>(orderRequestDTO);
+            await _orderRequestService.CreateOrderRequestAsync(orderRequest, cancellationToken);
+            return _mapper.Map<OrderRequestDto>(orderRequest);
+        }
+
+        public async Task UpdateOrderRequestStatusAsync(Guid orderRequestId, OrderRequestStatus newStatus, CancellationToken cancellationToken)
+        {
+            await _orderRequestService.UpdateOrderRequestStatusAsync(orderRequestId, newStatus, cancellationToken);
+        }
+
+        public async Task<OrderRequestDto> GetOrderRequestByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            var orderRequest = await _orderRequestRepository.GetByIdAsync(id, cancellationToken);
+            return _mapper.Map<OrderRequestDto>(orderRequest);
+        }
+    }
+}
