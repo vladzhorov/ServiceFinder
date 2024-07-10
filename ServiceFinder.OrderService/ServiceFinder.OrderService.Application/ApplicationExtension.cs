3b6ce9fadc14eaad9b599b9360876229c46362ca
vladzhorov <zhorovv7@gmail.com>
2024-07-10 18:50:50 +0300

    Update ApplicationExtension.cs

--- ServiceFinder.OrderService/ServiceFinder.OrderService.Application/ApplicationExtension.cs
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Application/ApplicationExtension.cs
@@ -5,6 +5,7 @@ using RabbitMQ.Client;
 using ServiceFinder.OrderService.Application.EventHandlers;
 using ServiceFinder.OrderService.Application.Interfaces;
 using ServiceFinder.OrderService.Application.Messaging;
+using ServiceFinder.OrderService.Application.Messaging.RabbitMQConfigurations;
 
 namespace ServiceFinder.OrderService.Application
 {
@@ -13,26 +14,40 @@ namespace ServiceFinder.OrderService.Application
         public static void AddApplication(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<RabbitMQConfiguration>(options => configuration.GetSection("RabbitMQ").Bind(options));
-            services.AddSingleton<IMessagePublisher>(provider =>
+            services.AddSingleton(provider =>
             {
-                var config = provider.GetRequiredService<IOptions<RabbitMQConfiguration>>().Value;
+                var options = provider.GetRequiredService<IOptions<RabbitMQConfiguration>>().Value;
                 var factory = new ConnectionFactory()
                 {
-                    HostName = config.HostName,
-                    UserName = config.UserName,
-                    Password = config.Password
+                    HostName = options.Connection!.HostName,
+                    UserName = options.Connection.UserName,
+                    Password = options.Connection.Password
                 };
-
                 var connection = factory.CreateConnection();
                 var channel = connection.CreateModel();
 
-                return new MessagePublisher(connection, channel, config.ExchangeName, config.QueueName, config.RoutingKey);
+                channel.ExchangeDeclare(exchange: options.Exchange!.Name, type: options.Exchange.Type);
+                channel.QueueDeclare(queue: options.Queue!.Name,
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+                channel.QueueBind(queue: options.Queue.Name,
+                                  exchange: options.Exchange.Name,
+                                  routingKey: options.Queue.RoutingKey);
+
+                return channel;
             });
 
+            services.AddScoped<IMessagePublisher, MessagePublisher>();
+        }
+
+        public static void AddApplication(this IServiceCollection services)
+        {
             services.AddScoped<IOrderAppService, OrderAppService>();
             services.AddScoped<IOrderRequestAppService, OrderRequestAppService>();
-            services.AddSingleton<OrderRequestStatusChangedEventHandler>();
-            services.AddSingleton<OrderStatusChangedEventHandler>();
+            services.AddScoped<OrderRequestStatusChangedEventHandler>();
+            services.AddScoped<OrderStatusChangedEventHandler>();
         }
     }
 }
