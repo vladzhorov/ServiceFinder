5d1766a2fc3e415dd2c77095dbf079b6e38f6fee
vladzhorov <zhorovv7@gmail.com>
2024-07-10 18:50:55 +0300

    Update MessagePublisher.cs

--- ServiceFinder.OrderService/ServiceFinder.OrderService.Application/Messaging/MessagePublisher.cs
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Application/Messaging/MessagePublisher.cs
@@ -5,31 +5,15 @@ namespace ServiceFinder.OrderService.Application.Messaging
 {
     public class MessagePublisher : IMessagePublisher
     {
-        private readonly IConnection _connection;
         private readonly IModel _channel;
         private readonly string _exchangeName;
-        private readonly string _queueName;
         private readonly string _routingKey;
 
-        public MessagePublisher(IConnection connection, IModel channel, string exchangeName, string queueName, string routingKey)
+        public MessagePublisher(IModel channel, string exchangeName, string routingKey)
         {
-            _connection = connection;
             _channel = channel;
             _exchangeName = exchangeName;
-            _queueName = queueName;
             _routingKey = routingKey;
-
-            _channel.ExchangeDeclare(exchange: _exchangeName, type: "direct");
-
-            _channel.QueueDeclare(queue: _queueName,
-                                 durable: false,
-                                 exclusive: false,
-                                 autoDelete: false,
-                                 arguments: null);
-
-            _channel.QueueBind(queue: _queueName,
-                              exchange: _exchangeName,
-                              routingKey: _routingKey);
         }
 
         public void Publish(string message)
@@ -41,11 +25,9 @@ namespace ServiceFinder.OrderService.Application.Messaging
                                  body: body);
             Console.WriteLine($" [x] Sent {message}");
         }
-
         public void Dispose()
         {
             _channel.Close();
-            _connection.Close();
         }
     }
 }
