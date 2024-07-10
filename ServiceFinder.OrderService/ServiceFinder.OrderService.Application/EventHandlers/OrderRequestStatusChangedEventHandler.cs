21a9f940ed91ffcbfeef56862f62e9f73590fd7d
vladzhorov <zhorovv7@gmail.com>
2024-07-10 13:46:14 +0300

    add eventHandlers

---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Application/EventHandlers/OrderRequestStatusChangedEventHandler.cs
@@ -0,0 +1,22 @@
+﻿using ServiceFinder.OrderService.Application.Messaging;
+using ServiceFinder.OrderService.Domain.Events;
+using System.Text.Json;
+
+namespace ServiceFinder.OrderService.Application.EventHandlers
+{
+    public class OrderRequestStatusChangedEventHandler
+    {
+        private readonly MessagePublisher _publisher;
+
+        public OrderRequestStatusChangedEventHandler(MessagePublisher publisher)
+        {
+            _publisher = publisher;
+        }
+
+        public void Handle(OrderRequestStatusChangedEvent domainEvent)
+        {
+            var message = JsonSerializer.Serialize(domainEvent);
+            _publisher.Publish(message);
+        }
+    }
+}
---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Application/EventHandlers/OrderStatusChangedEventHandler .cs
@@ -0,0 +1,22 @@
+﻿using ServiceFinder.OrderService.Application.Messaging;
+using ServiceFinder.OrderService.Domain.Events;
+using System.Text.Json;
+
+namespace ServiceFinder.OrderService.Application.EventHandlers
+{
+    public class OrderStatusChangedEventHandler
+    {
+        private readonly MessagePublisher _publisher;
+
+        public OrderStatusChangedEventHandler(MessagePublisher publisher)
+        {
+            _publisher = publisher;
+        }
+
+        public void Handle(OrderStatusChangedEvent domainEvent)
+        {
+            var message = JsonSerializer.Serialize(domainEvent);
+            _publisher.Publish(message);
+        }
+    }
+}
