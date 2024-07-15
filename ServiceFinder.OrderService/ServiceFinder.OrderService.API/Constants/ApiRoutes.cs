21cf37ad6ddc7f714b6d0601bf783659ed7beb20
vladzhorov <zhorovv7@gmail.com>
2024-07-15 18:14:24 +0300

    add error/validation/api constants

---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.API/Constants/ApiRoutes.cs
@@ -0,0 +1,11 @@
+﻿namespace ServiceFinder.OrderService.API.Constants
+{
+    public static class ApiRoutes
+    {
+        public const string Api = "/api";
+        public const string id = "{id}";
+        public const string Orders = Api + "/orders";
+        public const string OrderRequests = Api + "/order-requests";
+        public const string Status = id + "/status";
+    }
+}
