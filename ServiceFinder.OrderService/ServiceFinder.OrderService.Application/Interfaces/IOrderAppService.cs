3cff84fd02969e2c69611a4bc4f438ebfeae59e0
vladzhorov <zhorovv7@gmail.com>
2024-07-15 17:43:38 +0300

    add get all signature

--- ServiceFinder.OrderService/ServiceFinder.OrderService.Application/Interfaces/IOrderAppService.cs
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Application/Interfaces/IOrderAppService.cs
@@ -1,4 +1,4 @@
-﻿using ServiceFinder.OrderService.Application.DTOs;
+﻿using ServiceFinder.Domain.PaginationObjects;
 using ServiceFinder.OrderService.Domain.Enums;
 
 namespace ServiceFinder.OrderService.Application.Interfaces
@@ -8,5 +8,6 @@ namespace ServiceFinder.OrderService.Application.Interfaces
         Task<OrderDto> CreateOrderAsync(OrderDto orderDTO, decimal baseRatePerMinute, int baseRateDurationInMinutes, CancellationToken cancellationToken);
         Task UpdateOrderStatusAsync(Guid orderId, OrderStatus newStatus, CancellationToken cancellationToken);
         Task<OrderDto> GetOrderByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task<PagedResult<OrderDto>> GetAllOrderAsync(int pageNumber, int pageSize);
     }
 }
