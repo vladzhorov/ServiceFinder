bd9b94fe2537a66bdde63829fe6bf9aaf99f0a0b
vladzhorov <zhorovv7@gmail.com>
2024-07-15 18:12:36 +0300

    add viewModels

---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.API/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace ServiceFinder.OrderService.API.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? ErrorCode { get; set; }
+        public string? Message { get; set; }
+
+    }
+}
---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.API/ViewModels/Order/OrderViewModel.cs
@@ -0,0 +1,14 @@
+﻿using ServiceFinder.OrderService.Domain.Enums;
+
+namespace ServiceFinder.OrderService.API.ViewModels.Order
+{
+    public class OrderViewModel
+    {
+        public Guid Id { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public OrderStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
