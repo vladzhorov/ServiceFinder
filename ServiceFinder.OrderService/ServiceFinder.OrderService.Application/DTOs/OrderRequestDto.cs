9cfa90d7f78597900556f8e749f06ff8e7f19bb8
vladzhorov <zhorovv7@gmail.com>
2024-07-15 17:36:24 +0300

    update dto

--- ServiceFinder.OrderService/ServiceFinder.OrderService.Application/DTOs/OrderRequestDto.cs
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Application/DTOs/OrderRequestDto.cs
@@ -5,7 +5,7 @@ namespace ServiceFinder.OrderService.Application.DTOs
     public class OrderRequestDto
     {
         public Guid Id { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public OrderRequestStatus Status { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
