d6a18a9f8adcd29e2a52ef49880060e3d29d3ee7
vladzhorov <zhorovv7@gmail.com>
2024-07-15 17:42:44 +0300

    Revert "update dto"
    This reverts commit 9cfa90d7f78597900556f8e749f06ff8e7f19bb8.

--- ServiceFinder.OrderService/ServiceFinder.OrderService.Application/DTOs/OrderDto.cs
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Application/DTOs/OrderDto.cs
@@ -1,14 +1,15 @@
 ﻿using ServiceFinder.OrderService.Domain.Enums;
 
-namespace ServiceFinder.OrderService.Application.DTOs
+public class OrderDto
 {
-    public class OrderDto
-    {
-        public Guid Id { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public OrderStatus Status { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-    }
+    public Guid Id { get; set; }
+    public Guid CustomerId { get; set; }
+    public Guid ServiceId { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public OrderStatus Status { get; set; }
+    public int DurationInMinutes { get; set; }
+    public DateTime ScheduledDate { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
 }
