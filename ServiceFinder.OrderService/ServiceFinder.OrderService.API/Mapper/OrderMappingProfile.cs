8ba0294393d26eaa8db5c7dc586ed627543ce2ee
vladzhorov <zhorovv7@gmail.com>
2024-07-15 18:13:45 +0300

    Create OrderMappingProfile.cs

---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.API/Mapper/OrderMappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using ServiceFinder.API.ViewModels.OrderRequest;
+using ServiceFinder.Domain.PaginationObjects;
+using ServiceFinder.OrderService.API.ViewModels.Order;
+using ServiceFinder.OrderService.Application.DTOs;
+
+namespace ServiceFinder.OrderService.API.Mapper
+{
+    public class OrderViewModelMappingProfile : Profile
+    {
+        public OrderViewModelMappingProfile()
+        {
+            CreateMap<OrderDto, OrderViewModel>().ReverseMap();
+            CreateMap<CreateOrderViewModel, OrderDto>();
+
+            CreateMap<OrderRequestDto, OrderRequestViewModel>().ReverseMap();
+            CreateMap<CreateOrderRequestViewModel, OrderRequestDto>();
+
+            CreateMap(typeof(PagedResult<>), typeof(PagedResult<>));
+        }
+    }
+}
