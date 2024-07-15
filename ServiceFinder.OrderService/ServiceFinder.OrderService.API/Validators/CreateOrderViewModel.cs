134912fa84fb09d6579a56de7ef0f0d743c460f7
vladzhorov <zhorovv7@gmail.com>
2024-07-15 18:14:39 +0300

    add validators

---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.API/Validators/CreateOrderViewModel.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using ServiceFinder.API.Constants;
+using ServiceFinder.OrderService.API.ViewModels.Order;
+
+namespace ServiceFinder.API.Validators
+{
+    public class CreateOrderViewModelValidator : AbstractValidator<CreateOrderViewModel>
+    {
+        public CreateOrderViewModelValidator()
+        {
+            RuleFor(x => x.ServiceId).NotEmpty();
+            RuleFor(x => x.CustomerId).NotEmpty();
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.ScheduledDate).NotEmpty();
+            RuleFor(x => x.DurationInMinutes).NotEmpty();
+            RuleFor(x => x.BaseRatePerMinute).GreaterThan(ValidationConstants.BaseRatePerMinuteMinValue);
+            RuleFor(x => x.BaseRateDurationInMinutes).GreaterThan(ValidationConstants.BaseRateDurationInMinutesMinValue);
+        }
+    }
+}
