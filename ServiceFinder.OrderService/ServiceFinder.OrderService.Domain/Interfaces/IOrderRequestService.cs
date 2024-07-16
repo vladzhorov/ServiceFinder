80a4c460bca57d2183a3ff7ba74c652a6e467dc9
vladzhorov <zhorovv7@gmail.com>
2024-07-16 12:47:51 +0300

    add domain service interfaces

---
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Domain/Interfaces/IOrderRequestService.cs
@@ -0,0 +1,11 @@
+﻿using ServiceFinder.OrderService.Domain.Enums;
+using ServiceFinder.OrderService.Domain.Models;
+
+namespace ServiceFinder.OrderService.Domain.Interfaces
+{
+    public interface IOrderRequestService
+    {
+        Task UpdateOrderRequestStatusAsync(Guid orderRequestId, OrderRequestStatus newStatus, CancellationToken cancellationToken);
+        Task CreateOrderRequestAsync(OrderRequest orderRequest, CancellationToken cancellationToken);
+    }
+}
