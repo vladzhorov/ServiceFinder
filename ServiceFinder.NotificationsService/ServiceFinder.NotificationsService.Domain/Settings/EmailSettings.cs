1b152c0eec8337f1e011751a3fe4929d400adbd2
vladzhorov <zhorovv7@gmail.com>
2024-07-18 11:20:22 +0300

    add email components

---
+++ ServiceFinder.NotificationsService/ServiceFinder.NotificationsService.Domain/Settings/EmailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace ServiceFinder.NotificationsService.Domain.Settings
+{
+    public class EmailSettings
+    {
+        public string? SenderEmail { get; set; }
+        public string? Host { get; set; }
+        public int Port { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+    }
+}
---
+++ ServiceFinder.NotificationsService/ServiceFinder.NotificationsService.Infrastructure/EmailService/EmailSender.cs
@@ -0,0 +1,35 @@
+﻿// Infrastructure/Services/EmailSender.cs
+using Microsoft.Extensions.Options;
+using ServiceFinder.NotificationService.Domain.Interfaces;
+using ServiceFinder.NotificationsService.Domain.Settings;
+using System.Net;
+using System.Net.Mail;
+
+namespace ServiceFinder.NotificationService.Infrastructure.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        private readonly SmtpClient _smtpClient;
+        private readonly EmailSettings _emailSettings;
+
+        public EmailSender(IOptions<EmailSettings> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+            _smtpClient = new SmtpClient(_emailSettings.Host)
+            {
+                Port = _emailSettings.Port,
+                Credentials = new NetworkCredential(_emailSettings.Username, _emailSettings.Password),
+                EnableSsl = true
+            };
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string message)
+        {
+            var mailMessage = new MailMessage(_emailSettings.SenderEmail, email, subject, message)
+            {
+                IsBodyHtml = true
+            };
+            await _smtpClient.SendMailAsync(mailMessage);
+        }
+    }
+}
