cd9a53d81dffb43720968e389a76156004847c2e
vladzhorov <zhorovv7@gmail.com>
2024-07-18 11:20:52 +0300

    add notification components

---
+++ ServiceFinder.NotificationsService/ServiceFinder.NotificationsService.Infrastructure/NotificationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServiceFinder.NotificationsService.Domain.Models;
+
+public class NotificationDbContext : DbContext
+{
+    public NotificationDbContext(DbContextOptions<NotificationDbContext> options) : base(options) { }
+
+    public DbSet<Notification> Notifications { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ ServiceFinder.NotificationsService/ServiceFinder.NotificationsService.Infrastructure/RabbitMQ/NotificationConsumer.cs
@@ -0,0 +1,28 @@
+﻿using MassTransit;
+using ServiceFinder.NotificationsService.Domain.Events;
+using ServiceFinder.NotificationsService.Domain.Interfaces;
+
+namespace ServiceFinder.NotificationService.Infrastructure.RabbitMQ
+{
+    public class NotificationConsumer :
+        IConsumer<OrderCreatedEvent>,
+        IConsumer<OrderUpdatedEvent>
+    {
+        private readonly INotificationService _notificationService;
+
+        public NotificationConsumer(INotificationService notificationService)
+        {
+            _notificationService = notificationService;
+        }
+
+        public async Task Consume(ConsumeContext<OrderCreatedEvent> context)
+        {
+            await _notificationService.SendNotificationAsync(context.Message);
+        }
+
+        public async Task Consume(ConsumeContext<OrderUpdatedEvent> context)
+        {
+            await _notificationService.SendNotificationAsync(context.Message);
+        }
+    }
+}
