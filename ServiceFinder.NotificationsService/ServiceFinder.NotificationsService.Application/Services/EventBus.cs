b24dba039a3012cdff8d628df8b23e62136fa910
vladzhorov <zhorovv7@gmail.com>
2024-07-18 17:56:00 +0300

    Create EventBus.cs

---
+++ ServiceFinder.NotificationsService/ServiceFinder.NotificationsService.Application/Services/EventBus.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using ServiceFinder.NotificationService.Application.Interfaces;
+
+namespace ServiceFinder.NotificationService.Application.Services
+{
+    public class EventBus : IEventBus
+    {
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public EventBus(IPublishEndpoint publishEndpoint)
+        {
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public Task PublishAsync<T>(T message) where T : class
+        {
+            return _publishEndpoint.Publish(message);
+        }
+    }
+}
