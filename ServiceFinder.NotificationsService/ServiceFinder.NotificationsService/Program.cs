fc4e52c7b042cfb266e0d3e7a781eca6597520d9
vladzhorov <zhorovv7@gmail.com>
2024-07-18 17:56:59 +0300

    Create Program.cs

---
+++ ServiceFinder.NotificationsService/ServiceFinder.NotificationsService/Program.cs
@@ -0,0 +1,49 @@
+using Serilog;
+using ServiceFinder.NotificationService.Application.BackgroundServices;
+using ServiceFinder.NotificationService.Application.DI;
+using ServiceFinder.NotificationService.Infrastructure.DI;
+
+
+namespace ServiceFinder.NotificationService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting application host.");
+                CreateHostBuilder(args, configuration).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application host terminated unexpectedly.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args, IConfiguration configuration) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddInfrastructureServices(configuration);
+                    services.AddApplicationServices(configuration);
+                    services.AddHostedService<NotificationListener>();
+                });
+
+    }
+}
