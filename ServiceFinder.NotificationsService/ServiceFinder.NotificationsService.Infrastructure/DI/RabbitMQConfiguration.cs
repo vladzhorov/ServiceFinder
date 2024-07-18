9063c201042d0b67fdaa3bbde34608fb8ab6d6c9
vladzhorov <zhorovv7@gmail.com>
2024-07-18 11:20:29 +0300

    Create RabbitMQConfiguration.cs

---
+++ ServiceFinder.NotificationsService/ServiceFinder.NotificationsService.Infrastructure/DI/RabbitMQConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceFinder.NotificationsService.Infrastructure.Constants;
+
+namespace ServiceFinder.NotificationService.Infrastructure.RabbitMQ
+{
+    public static class RabbitMQConfiguration
+    {
+        public static IServiceCollection AddRabbitMQ(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<NotificationConsumer>();
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host(configuration[RabbitMqConstants.Host], h =>
+                    {
+                        h.Username(configuration[RabbitMqConstants.Username]);
+                        h.Password(configuration[RabbitMqConstants.Password]);
+                    });
+
+                    cfg.ReceiveEndpoint(RabbitMqConstants.NotificationQueue, e =>
+                    {
+                        e.ConfigureConsumer<NotificationConsumer>(context);
+                    });
+                });
+            });
+
+            services.AddMassTransitHostedService();
+            return services;
+        }
+    }
+}
