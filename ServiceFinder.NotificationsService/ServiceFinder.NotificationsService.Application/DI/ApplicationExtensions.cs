98180ad2a80ed227a0aae1eef39def5eebcf4a23
vladzhorov <zhorovv7@gmail.com>
2024-07-19 11:47:39 +0300

    Update ApplicationExtensions.cs

--- ServiceFinder.NotificationsService/ServiceFinder.NotificationsService.Application/DI/ApplicationExtensions.cs
+++ ServiceFinder.NotificationsService/ServiceFinder.NotificationsService.Application/DI/ApplicationExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ServiceFinder.NotificationService.Application.Consumers;
-using ServiceFinder.NotificationService.Application.Interfaces;
-using ServiceFinder.NotificationService.Application.Services;
 using ServiceFinder.NotificationsService.Domain.Interfaces;
 
 namespace ServiceFinder.NotificationService.Application.DI
@@ -12,7 +10,6 @@ namespace ServiceFinder.NotificationService.Application.DI
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddTransient<INotificationService, Services.NotificationService>();
-            services.AddTransient<IEventBus, EventBus>();
             services.AddScoped<NotificationConsumer>();
 
             return services;
