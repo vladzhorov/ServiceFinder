350032fe2a7561964d50fc9db6082fd7a9c99fb8
vladzhorov <zhorovv7@gmail.com>
2024-07-29 00:29:23 +0300

    add separate notification consumers

---
+++ ServiceFinder.NotificationsService/ServiceFinder.NotificationsService.Infrastructure/RabbitMQ/OrderStatusChangedEventConsumer.cs
@@ -0,0 +1,31 @@
+﻿using MassTransit;
+using MediatR;
+using ServiceFinder.NotificationsService.Domain.Events;
+using ServiceFinder.Shared.Events;
+
+namespace ServiceFinder.NotificationService.Application.Consumers
+{
+    public class OrderStatusChangedEventConsumer :
+        IConsumer<OrderStatusChangedEvent>
+    {
+        private readonly IMediator _mediator;
+
+        public OrderStatusChangedEventConsumer(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        public async Task Consume(ConsumeContext<OrderStatusChangedEvent> context)
+        {
+            try
+            {
+                await _mediator.Send(new SendEmailCommand(context.Message.Email!, "Order Created", $"Your order with ID {context.Message.OrderId} was created"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to process OrderStatusChangedEvent: {ex.Message}");
+                throw;
+            }
+        }
+    }
+}
