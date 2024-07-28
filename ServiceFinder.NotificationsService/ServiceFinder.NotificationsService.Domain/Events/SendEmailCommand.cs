c751076b1dadf7e157194ab015fa0364dfa88fc4
vladzhorov <zhorovv7@gmail.com>
2024-07-29 00:30:46 +0300

    Create SendEmailCommand.cs

---
+++ ServiceFinder.NotificationsService/ServiceFinder.NotificationsService.Domain/Events/SendEmailCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+
+namespace ServiceFinder.NotificationsService.Domain.Events
+{
+    public class SendEmailCommand : IRequest<Unit>
+    {
+        public string Email { get; }
+        public string Subject { get; }
+        public string Body { get; }
+
+        public SendEmailCommand(string email, string subject, string body)
+        {
+            Email = email;
+            Subject = subject;
+            Body = body;
+        }
+    }
+}
