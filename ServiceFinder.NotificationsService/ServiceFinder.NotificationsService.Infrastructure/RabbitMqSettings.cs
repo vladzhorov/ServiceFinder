1284186968c082cc5715fe2d9c0ff3363cb135f9
vladzhorov <zhorovv7@gmail.com>
2024-07-29 00:30:38 +0300

    Create RabbitMqSettings.cs

---
+++ ServiceFinder.NotificationsService/ServiceFinder.NotificationsService.Infrastructure/RabbitMqSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ServiceFinder.NotificationsService.Infrastructure
+{
+    public class RabbitMqSettings
+    {
+        public string Host { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
