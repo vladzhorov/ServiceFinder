add0e0fd92fe3252e5928f09b5b07461bd5159c3
vladzhorov <zhorovv7@gmail.com>
2024-07-29 00:21:56 +0300

    add mediator

--- ServiceFinder.NotificationsService/ServiceFinder.NotificationsService.Application/DI/ApplicationExtensions.cs
+++ ServiceFinder.NotificationsService/ServiceFinder.NotificationsService.Application/DI/ApplicationExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using ServiceFinder.NotificationService.Application.Consumers;
+using ServiceFinder.NotificationsService.Application.EventHandlers;
 using ServiceFinder.NotificationsService.Domain.Interfaces;
 
 namespace ServiceFinder.NotificationService.Application.DI
@@ -9,8 +9,9 @@ namespace ServiceFinder.NotificationService.Application.DI
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<OrderRequestStatusChangedEventHandler>());
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<OrderStatusChangedEventHandler>());
             services.AddTransient<INotificationService, Services.NotificationService>();
-            services.AddScoped<NotificationConsumer>();
 
             return services;
         }
