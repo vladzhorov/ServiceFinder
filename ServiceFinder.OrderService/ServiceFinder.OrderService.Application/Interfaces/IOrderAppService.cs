6639b92db77d598a7031403c568e025c9c8cff70
vladzhorov <zhorovv7@gmail.com>
2024-07-29 20:07:48 +0300

    add shared projects reference

--- ServiceFinder.OrderService/ServiceFinder.OrderService.Application/Interfaces/IOrderAppService.cs
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Application/Interfaces/IOrderAppService.cs
@@ -1,12 +1,12 @@
 ﻿using ServiceFinder.Domain.PaginationModels;
-using ServiceFinder.OrderService.Domain.Enums;
+using ServiceFinder.Shared.Enums;
 
 namespace ServiceFinder.OrderService.Application.Interfaces
 {
     public interface IOrderAppService
     {
         Task<OrderDto> CreateOrderAsync(OrderDto orderDTO, decimal baseRatePerMinute, int baseRateDurationInMinutes, CancellationToken cancellationToken);
-        Task UpdateOrderStatusAsync(Guid orderId, OrderStatus newStatus, CancellationToken cancellationToken);
+        Task UpdateOrderStatusAsync(Guid orderId, OrderStatus newStatus, string email, CancellationToken cancellationToken);
         Task<OrderDto> GetOrderByIdAsync(Guid id, CancellationToken cancellationToken);
         Task<PagedResult<OrderDto>> GetAllOrderAsync(int pageNumber, int pageSize, CancellationToken cancellationToken);
     }
