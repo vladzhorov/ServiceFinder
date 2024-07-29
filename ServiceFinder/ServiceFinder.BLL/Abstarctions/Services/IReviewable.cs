ff30d812b4f11337ffc2ae56693281346c76cc51
vladzhorov <108758611+vladzhorov@users.noreply.github.com>
2024-07-29 13:09:54 +0300

    Delete ServiceFinder/ServiceFinder.BLL/Abstarctions/Services/IReviewable.cs

--- ServiceFinder/ServiceFinder.BLL/Abstarctions/Services/IReviewable.cs
+++
@@ -1,9 +0,0 @@
-﻿using ServiceFinder.BLL.Models;
-
-namespace ServiceFinder.BLL.Abstarctions.Services
-{
-    public interface IReviewable
-    {
-        public delegate ICollection<Review> ReviewsGetter<T>(T item);
-    }
-}
