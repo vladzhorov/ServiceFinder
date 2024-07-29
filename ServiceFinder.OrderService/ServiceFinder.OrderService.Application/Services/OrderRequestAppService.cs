fbc2e3855183bd780c6d7d95c78dcaa2f8a1b196
vladzhorov <zhorovv7@gmail.com>
2024-07-29 20:11:25 +0300

    update services

--- ServiceFinder.OrderService/ServiceFinder.OrderService.Application/Services/OrderRequestAppService.cs
+++ ServiceFinder.OrderService/ServiceFinder.OrderService.Application/Services/OrderRequestAppService.cs
@@ -2,20 +2,20 @@
 using ServiceFinder.Domain.PaginationModels;
 using ServiceFinder.OrderService.Application.DTOs;
 using ServiceFinder.OrderService.Application.Interfaces;
-using ServiceFinder.OrderService.Domain.Enums;
 using ServiceFinder.OrderService.Domain.Exceptions;
 using ServiceFinder.OrderService.Domain.Interfaces;
 using ServiceFinder.OrderService.Domain.Models;
-using ServiceFinder.OrderService.Domain.Services;
+using ServiceFinder.Shared.Enums;
+
 namespace ServiceFinder.OrderService.Application
 {
     public class OrderRequestAppService : IOrderRequestAppService
     {
-        private readonly OrderRequestService _orderRequestService;
+        private readonly IOrderRequestService _orderRequestService;
         private readonly IOrderRequestRepository _orderRequestRepository;
         private readonly IMapper _mapper;
 
-        public OrderRequestAppService(OrderRequestService orderRequestService, IOrderRequestRepository orderRequestRepository, IMapper mapper)
+        public OrderRequestAppService(IOrderRequestService orderRequestService, IOrderRequestRepository orderRequestRepository, IMapper mapper)
         {
             _orderRequestService = orderRequestService;
             _orderRequestRepository = orderRequestRepository;
@@ -29,9 +29,9 @@ namespace ServiceFinder.OrderService.Application
             return _mapper.Map<OrderRequestDto>(orderRequest);
         }
 
-        public Task UpdateOrderRequestStatusAsync(Guid orderRequestId, OrderRequestStatus newStatus, CancellationToken cancellationToken)
+        public Task UpdateOrderRequestStatusAsync(Guid orderRequestId, OrderRequestStatus newStatus, string email, CancellationToken cancellationToken)
         {
-            return _orderRequestService.UpdateOrderRequestStatusAsync(orderRequestId, newStatus, cancellationToken);
+            return _orderRequestService.UpdateOrderRequestStatusAsync(orderRequestId, newStatus, email, cancellationToken);
         }
 
         public async Task<OrderRequestDto> GetOrderRequestByIdAsync(Guid id, CancellationToken cancellationToken)
