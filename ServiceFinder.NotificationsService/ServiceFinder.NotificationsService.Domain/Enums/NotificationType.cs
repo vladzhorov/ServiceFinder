721802f2412ef2e0368d2b66e394d170e229857d
vladzhorov <zhorovv7@gmail.com>
2024-07-29 19:33:24 +0300

    delete not used components

--- ServiceFinder.NotificationsService/ServiceFinder.NotificationsService.Domain/Enums/NotificationType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ServiceFinder.NotificationsService.Domain.Enums
-{
-    public enum NotificationType
-    {
-        OrderCreated = 0,
-        OrderUpdated = 1,
-        OrderCanceled = 2
-    }
-}
